fc1b4bb123f4beee773b90057ef5d64c2a9b49fc
Emma Aguirre <90349262+Emmily808@users.noreply.github.com>
2023-06-27 14:24:13 -0300

    Add the full project.
    Add adjustable size, color selection, and painting functionality. 
    Add a simple interface.

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
-const mainContainer = document.getElementById("main-container");
-const colourModeButton = document.getElementById("colourModeButton")
-const eraserModeButton = document.getElementById("eraserModeButton");
+let color = "black";
+
 let slider = document.getElementById("slider");
+let result = document.getElementById("result");
 
+function showGridNumber() {
+  result.innerHTML = `${slider.value} x ${slider.value}`;
+}
 
-  slider.addEventListener('change', ()=>{
-    console.log(slider.value)
-    
-    
-    for (let i = 1; i <= slider.value; ++i) {
-      
-      const gridChildrens = document.createElement("div");
-      gridChildrens.setAttribute("id", `gridChildrens`);
-      mainContainer.appendChild(gridChildrens);
-      
-      
-      
-      gridChildrens.addEventListener("mouseover", () => {
-        gridChildrens.setAttribute("class", "gridChildrensColoured");
-      });
-      
-      eraserModeButton.addEventListener("click", () => {
-        gridChildrens.addEventListener("mouseover", () => {
-          gridChildrens.setAttribute("class", "gridChildrensErased");
-        });
-      });
-      
-      colourModeButton.addEventListener('click',()=>{
-        gridChildrens.addEventListener("mouseover", () => {
-          gridChildrens.setAttribute("class", "gridChildrensColoured");
-        });
-      })
-    }
-    
-  })
+showGridNumber();
+
+slider.addEventListener("mousemove", showGridNumber);
+
+function createGrid(size) {
+  let mainContainer = document.getElementById("main-container");
+  let childrensContainer = mainContainer.querySelectorAll("div");
+  childrensContainer.forEach((div) => div.remove());
+  mainContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  mainContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let gridChildrens = document.createElement("div");
+    gridChildrens.style.backgroundColor = "white";
+    mainContainer.appendChild(gridChildrens);
+    gridChildrens.addEventListener("mouseover", gridColor);
+  }
+}
+
+createGrid(16);
+
+function deleteGrid(input) {
+  createGrid(input);
+}
+
+function gridColor() {
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetGrid() {
+  let mainContainer = document.getElementById("main-container");
+  let childrensContainer = mainContainer.querySelectorAll("div");
+  childrensContainer.forEach((div) => (div.style.backgroundColor = "white"));
+}
